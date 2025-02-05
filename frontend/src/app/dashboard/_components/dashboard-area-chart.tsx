40b21a95f89d63cd3f1bdab796ab3aa6e17102e3
mivashchenko.work <mivashchenko.work@gmail.com>
2025-02-05 17:40:36 -0500

    update charts

--- frontend/src/app/dashboard/_components/dashboard-area-chart.tsx
+++ frontend/src/app/dashboard/_components/dashboard-area-chart.tsx
@@ -121,14 +121,14 @@ const chartData = [
 
 const chartConfig = {
   visitors: {
-    label: "Visitors",
+    label: "Messages",
   },
   desktop: {
-    label: "Desktop",
+    label: "High",
     color: "hsl(var(--chart-1))",
   },
   mobile: {
-    label: "Mobile",
+    label: "No issue",
     color: "hsl(var(--chart-2))",
   },
 } satisfies ChartConfig
@@ -154,9 +154,9 @@ export const  DashboardAreaChart = ()=>  {
     <Card>
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
-          <CardTitle>Area Chart - Interactive</CardTitle>
+          <CardTitle>Severity levels</CardTitle>
           <CardDescription>
-            Showing total visitors for the last 3 months
+            Showing total messages for the last 3 months
           </CardDescription>
         </div>
         <Select value={timeRange} onValueChange={setTimeRange}>
