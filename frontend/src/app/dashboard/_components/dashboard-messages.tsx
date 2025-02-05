3163475cd6965fc02957907ad57945a1424bb6f4
mivashchenko.work <mivashchenko.work@gmail.com>
2025-02-04 20:09:07 -0500

    a lot of changes

--- frontend/src/app/dashboard/_components/dashboard-messages.tsx
+++ frontend/src/app/dashboard/_components/dashboard-messages.tsx
@@ -176,13 +176,33 @@ export const DashboardMessages = () => {
           >
             {table.getHeaderGroups().map(renderHeaderRow)}
           </TableHeader>
-          <TableBody
-            // className={'grid relative'}
-            style={{
-              height: `${virtualizer.getTotalSize()}px`,
-            }}
-          >
-            {virtualizer.getVirtualItems().map(renderRow)}
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </div>
